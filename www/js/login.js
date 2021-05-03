48139f20902b62eaf56f024e9d8b81b4baee3a0b
AmadorMartinez10 <ttore10.amc@gmail.com>
2021-05-03 16:42:35 +0200

    LOGIN and REGISTER with API call function to Heroku

--- www/js/login.js
+++ www/js/login.js
@@ -1,9 +1,12 @@
 (function($){
     $(function(){
         loginSwap();
+        $('#loginButton').click(loginFunction);
         $('#registerButton').click(registerFunction);
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+const herokuUrl = "https://apilergens.herokuapp.com";
 
 function loginSwap(){
     const signUpButton = $('#signUp');
@@ -19,6 +22,13 @@ function loginSwap(){
     });
 }
 
+function loginFunction(){
+    var logEmail = $('#logEmail').val();
+    var logPassword = $('#logPassword').val();
+
+    logApiCall(logEmail, logPassword);
+}
+
 function registerFunction(){
     var name = $('#name').val();
     var surname1 = $('#surname1').val();
@@ -27,10 +37,59 @@ function registerFunction(){
     var dni = $('#dni').val();
     var phone = $('#phone').val();
     var regPassword = $('#regPassword').val();
-    console.log(name);
+
     if(name!=""&&surname1!=""&&surname2!=""&&regEmail!=""&&dni!=""&&phone!=""&&regPassword!=""){
-        
+        var newUser = JSON.parse('{"name":"'+name+'","surname1":"'+surname1+'","surname2":"'+surname2+'","dni":"'+dni+'","phone":"'+phone+'","password":"'+regPassword+'","email":"'+regEmail+'"}');
+        console.log(newUser);
+        regApiCall(newUser);
     } else {
         alert("Campos vacios!");
     }
+}
+
+function logApiCall(logEmail, logPass){
+    console.log(logEmail+"          "+logPass);
+    $.ajax({
+        method: "POST",
+        url: herokuUrl+"/login",
+        data:{
+            "user" :{
+                "email" : '"'+logEmail+'"',
+                "password" : '"'+logPass+'"'
+            }
+        }
+      }).done(function (msg) {
+          alert("LOGIN correctamente");
+      }).fail(function (data) {
+          alert("No se ha encontrado el usuario");
+      });
+}
+
+function regApiCall(user){
+    $.ajax({
+        method: "POST",
+        url: herokuUrl+"/register",
+        data:{
+            "user" : {
+                "name": user.name,
+                "surname1":user.surname1,
+                "surname2":user.surname2,
+                "dni":user.dni,
+                "phone":user.phone,
+                "password":user.password,
+                "email":user.email,
+                "allergies": [{
+                    "ID": null,
+                    "Type": ""
+                }],
+                "favourites": [{
+                    "id": null
+                }]
+            }
+        }
+      }).done(function (msg) {
+          alert("REGISTER correcto");
+      }).fail(function (data) {
+          console.log(data);
+      });
 }
